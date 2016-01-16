f5e7fcfd05de5ee21c9827eb82f1eaa81f239c55
bokuweb <bokuweb12@gmail.com>
2016-01-16 15:21:43 +0900

    Fix resizable box api

---
+++ example/src/example.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import SortablePane from '../../src';
+
+export default class Example extends Component{
+  render() {
+    return (<SortablePane />);
+  }
+}
