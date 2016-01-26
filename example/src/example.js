dcdab2ae409da5dc515965a44d31540954d1bab6
bokuweb <bokuweb12@gmail.com>
2016-01-27 08:54:43 +0900

    Fix example

--- example/src/example.js
+++ example/src/example.js
@@ -5,23 +5,19 @@ export default class Example extends Component{
   render() {
     return (
       <SortablePane
-         marginRight={10}>
-        <div
-           style={{
-             fontSize: "40px",
-             textAlign:"center",
-             paddingTop:"30px",
-             height:"400px",
-             border: "solid 1px #ccc"
-           }}>A</div>
-        <div
-           style={{
-             fontSize: "40px",
-             textAlign:"center",
-             paddingTop:"30px",
-             height:"300px",
-             border: "solid 1px #ccc"
-           }}>B</div>
+         marginRight={10}
+         customStyle={{
+           fontSize: "40px",
+           textAlign:"center",
+           paddingTop:"30px",
+           height:"400px",
+           border: "solid 1px #ccc",
+           borderRadius: "5px",
+         }}
+         >
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
       </SortablePane>
     );
   }
