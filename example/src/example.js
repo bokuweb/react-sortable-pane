2fc861684aa6d0cbce5333db2ec03bca3d5d168c
bokuweb <bokuweb12@gmail.com>
2016-03-12 15:05:38 +0900

    Refactor example

--- example/src/example.js
+++ example/src/example.js
@@ -1,86 +1,94 @@
-import React, {Component} from 'react';
-import SortablePane, {Pane} from '../../src';
+import React, { Component } from 'react';
+import SortablePane, { Pane } from '../../src';
 
 const style = {
-  fontSize: "40px",
-  textAlign:"center",
-  paddingTop:"60px",
-  height:"400px",
-  border: "solid 1px #ccc",
-  borderRadius: "5px",
-  backgroundColor: "#fff"
+  fontSize:'40px',
+  textAlign: 'center',
+  paddingTop: '60px',
+  height: '400px',
+  border: 'solid 1px #ccc',
+  borderRadius: '5px',
+  backgroundColor: '#fff',
 };
 
-let id = 0;
-
-export default class Example extends Component{
+export default class Example extends Component {
   constructor(props) {
     super(props);
+    this.id = 3;
     this.state = {
-      list : [
-          <Pane
-        id="a"
-        width={200}
-        height={500}
-        minWidth={100}
-        maxWidth={800}
-        minHeight={100}
-        style={style}
-          >
-          A
+      list: [
+        <Pane
+          id={1}
+          width={200}
+          height={500}
+          minWidth={100}
+          maxWidth={800}
+          minHeight={100}
+          style={style}
+        >
+          1
         </Pane>,
         <Pane
-           id="b"
-           width={300}
-           height={400}
-           minWidth={100}
-           minHeight={100}
-           style={style}
-           >
-          B
+          id={2}
+          width={300}
+          height={400}
+          minWidth={100}
+          minHeight={100}
+          style={style}
+        >
+          2
         </Pane>,
         <Pane
-           id="c"
-           width={100}
-           height={200}
-           minWidth={100}
-           minHeight={100}
-           style={style}
-           >
-          C
+          id={3}
+          width={100}
+          height={200}
+          minWidth={100}
+          minHeight={100}
+          style={style}
+        >
+          3
         </Pane>,
-      ]
-    }
+      ],
+    };
+    this.add = ::this.add;
+    this.remove = ::this.remove;
+    this.onResize = ::this.onResize;
   }
 
   onResize(i) {
-    console.log(`resize pane${i}`);
+    console.log(`on resize pane id ${i}`);
+  }
+
+  add() {
+    this.state.list.splice(1, 0,
+                           <Pane
+                             id={this.id++}
+                             width={50}
+                             height={100}
+                             minWidth={50}
+                             minHeight={100}
+                             style={style}
+                           >
+                             {this.id}
+                           </Pane>);
+    this.setState({ list: this.state.list });
+  }
+
+  remove() {
+    this.state.list.splice(1, 1);
+    this.setState({ list: this.state.list });
   }
 
   render() {
     return (
       <div>
-        <a onClick={() => {
-            this.state.list.splice(1, 0, <Pane
-                                              id={id++}
-                                              width={50}
-                                              height={100}
-                                              minWidth={50}
-                                              minHeight={100}
-                                              style={style}
-                                              >
-                                   {id}
-                                   </Pane>);
-            this.setState({list: this.state.list})}} >add</a>
-        <a onClick={() => {
-          this.state.list.splice(1, 1);
-          this.setState({list: this.state.list})}} >remove</a>
+        <a onClick={this.add}>add</a>
+        <a onClick={this.remove}>remove</a>
         <SortablePane
-           ref="pane" 
-           margin={10}
-           onResize={this.onResize.bind(this)}
-           onOrderChange={order => console.dir(order)}
-          >
+          margin={10}
+          onResize={this.onResize}
+          onOrderChange={order => console.dir(order)}
+        >
           {this.state.list}
         </SortablePane>
       </div>
