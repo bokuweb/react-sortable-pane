dac7837bc8320f75367732744dd9aa851c2bef0b
bokuweb <bokuweb12@gmail.com>
2017-08-01 15:27:16 +0900

    Fix

--- deploy-minor.sh
+++ deploy-minor.sh
@@ -5,7 +5,7 @@ git config --global user.email bokuweb12@gmail.com
 git config --global user.name bokuweb
 git remote --v
 git reset --hard HEAD
-
 npm version minor
+git push origin master
 git push --tags
 npm publish
