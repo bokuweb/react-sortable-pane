c0bcb74de5e246aadec74c0609c79270d51f8b04
bokuweb <bokuweb@users.noreply.github.com>
2017-08-01 15:07:48 +0900

    Update deproy-patch.sh

--- deproy-patch.sh
+++ deproy-patch.sh
@@ -3,7 +3,6 @@ git config --global -l
 git config --global user.email bokuweb12@gmail.com
 git config --global user.name bokuweb
 git remote --v
-
 git reset --hard HEAD
 npm version patch
 git push --tags
