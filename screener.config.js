35ae2a22053f2c54a368028d8d2a3c77f58aef30
bokuweb <bokuweb12@gmail.com>
2018-06-06 10:32:14 +0900

    chore: cleanup

--- screener.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  projectRepo: 'bokuweb/react-sortable-pane',
-  storybookConfigDir: '.storybook',
-  apiKey: 'bdceb221-2d1f-4d46-8f2a-c3be931969fa',
-  resolution: '1024x768'
-};
